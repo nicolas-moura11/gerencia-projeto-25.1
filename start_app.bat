ca95906d54a01064a44bcdb9b07b392d0d9b8ecc
nicolas-moura11 <nicolasmoura36@gmail.com>
2025-06-06 17:33:47 -0300

    auto install

--- start_app.bat
+++ start_app.bat
@@ -1,8 +1,34 @@
 @echo off
-echo Iniciando os servicos da aplicacao (PostgreSQL e FastAPI)...
+title Inicializando do Receita Facil...
+
+echo ==========================================
+echo Iniciando os servicos da aplicacao:
+echo - PostgreSQL
+echo - FastAPI
+echo ==========================================
 echo.
 
 REM 
+docker --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo [ERRO] Docker não esta instalado ou nao esta no PATH.
+    pause
+    exit /b
+)
+
+REM 
+echo [INFO] Construindo e iniciando os containers com docker-compose...
 docker-compose up --build -d
+
+IF ERRORLEVEL 1 (
+    echo [ERRO] Falha ao iniciar os containers.
+    pause
+    exit /b
+)
+
+echo.
+echo [OK] Servicos iniciados com sucesso!
+echo Voce pode acessar em: http://localhost:8070/home
+echo.
 
 pause
